13e9fbca27b282641327164d514c28fdbdceea4b
Alberinea <kan9870@gmail.com>
2021-07-14 05:03:41 +0900

    Add very basic drag&drop function

--- main.js
+++ main.js
@@ -1,9 +1,92 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/dom.ts":
+/*!********************!*\
+  !*** ./src/dom.ts ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+var warships = document.querySelectorAll('.warship');
+var playerBoard = document.querySelector('.mainBoard');
+function drop() {
+    warships.forEach(function (warship) {
+        return warship.addEventListener('dragstart', function (e) {
+            e.dataTransfer.setData('text', e.target.className);
+        });
+    });
+    warships.forEach(function (warship) {
+        return warship.addEventListener('dragend', function (e) {
+            e.target.style.display = 'none';
+        });
+    });
+    playerBoard.addEventListener('drop', function (e) {
+        var data = e.dataTransfer.getData('text');
+        e.target.className += " " + data;
+    });
+    playerBoard.addEventListener('dragover', function (e) { return e.preventDefault(); });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (drop);
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
+/* harmony import */ var _dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dom */ "./src/dom.ts");
 
 function CreateShip(length) {
     var HP = [];
@@ -71,10 +154,14 @@ function CreateGameBoard(name) {
     };
 }
 var player1 = CreateGameBoard('player1');
-var player2 = CreateGameBoard('player2');
+// const player2 = CreateGameBoard('player2');
+console.log(player1);
 document
     .querySelectorAll('.grid')
     .forEach(function (grid) { return grid.addEventListener('click', function (e) { return getCoordinates(e, 5); }); });
+(0,_dom__WEBPACK_IMPORTED_MODULE_0__.default)();
+
+})();
 
 /******/ })()
 ;
